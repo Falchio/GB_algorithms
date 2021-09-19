2836013c09120f63a055c26d7dc2265ed5db79e7
Falchio <soulsard@gmail.com>
2021-09-19 12:33:22 +0300

    add revert function

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -2,43 +2,17 @@ package lesson3;
 
 public class Main {
     public static void main(String[] args) {
-//        MyStack<Integer> myStack = new MyStack<>();
-//
-//        myStack.push(5);
-//        myStack.push(2);
-//        myStack.push(9);
-//        myStack.push(1);
-//        myStack.push(4);
-//
-//        for (int i = 0; i < 5; i++) {
-//            System.out.println(myStack.pop());
-//        }
+        String[] array = {"Съешь", "еще", "этих", "мягких", "французких", "булок"};
+        revertArrayString(array);
+    }
 
-//        Expression e = new Expression("()+{}+[{}]");
-//        System.out.println(e.checkBracket());
-
-
-//        MyQueue<Integer> queue = new MyQueue<>();
-//
-//        queue.insert(3);
-//        queue.insert(2);
-//        queue.insert(7);
-//        queue.insert(4);
-//
-//        for (int i = 0; i < 4; i++) {
-//            System.out.println(queue.remove());
-//        }
-
-        MyPriorityQueue<Integer> mpq = new MyPriorityQueue<>();
-
-        mpq.insert(5);
-        mpq.insert(2);
-        mpq.insert(20);
-        mpq.insert(21);
-        mpq.insert(4);
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println(mpq.remove());
+    private static void revertArrayString(String[] array){
+        MyStack<String> stack = new MyStack<>();
+        for (String item: array) {
+            stack.push(item);
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println( stack.pop());
         }
     }
 }
